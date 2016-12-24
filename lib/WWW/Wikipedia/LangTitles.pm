bb094e67f7da1a401e1770f7384daa295f426ffd
Ben Bullock <benkasminbullock@gmail.com>
2016-12-24 17:58:01 +0900

    Initial commit of www-wikipedia-langtitles

---
+++ lib/WWW/Wikipedia/LangTitles.pm
@@ -0,0 +1,12 @@
+package WWW::Wikipedia::LangTitles;
+use warnings;
+use strict;
+use Carp;
+require Exporter;
+our @ISA = qw(Exporter);
+our @EXPORT_OK = qw//;
+our %EXPORT_TAGS = (
+    all => \@EXPORT_OK,
+);
+our $VERSION = '0.01';
+1;
---
+++ t/www-wikipedia-langtitles.t
@@ -0,0 +1,11 @@
+# This is a test for module WWW::Wikipedia::LangTitles.
+
+use warnings;
+use strict;
+use Test::More;
+use WWW::Wikipedia::LangTitles;
+
+done_testing ();
+# Local variables:
+# mode: perl
+# End:
